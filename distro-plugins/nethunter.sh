31469df39ff14ce7d148d8406079a9bcf24ad208
jdstryn <36963160+jdstryn@users.noreply.github.com>
2022-09-29 20:52:56 +0800

    Add files via upload

--- distro-plugins/nethunter.sh
+++ distro-plugins/nethunter.sh
@@ -6,8 +6,3 @@ DISTRO_NAME="nethunter"
 DISTRO_COMMENT="Only for arm64 device."
 TARBALL_URL['aarch64']="https://kali.download/nethunter-images/kali-2022.3/rootfs/kalifs-arm64-minimal.tar.xz"
 TARBALL_SHA256['aarch64']="6e07c219fd3228d7c018450c50a26b1ab4af1c5e4c4df46e0ea86ecbefddb0a6"
-
-distro_setup() {
-# Fixing sudo permission fo kali
-run_proot_cmd echo "kali ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/kali
-}
