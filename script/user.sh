7f275335a72e4579b75ab1d5f97e854e504ed5fb
jdstryn <36963160+jdstryn@users.noreply.github.com>
2022-09-29 15:31:30 +0800

    Add files via upload

---
+++ script/user.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+R="$(printf '\033[1;31m')"
+G="$(printf '\033[1;32m')"
+Y="$(printf '\033[1;33m')"
+W="$(printf '\033[1;37m')"
+C="$(printf '\033[1;36m')"
+
+banner() {
+    clear
+    printf "\033[0m\n"
+    printf "     \033[32mA modded version of nethunter for Termux\033[0m\n"
+    printf "\033[0m\n"
+
+}
+
+sudo() {
+    echo -e "\n${R} [${W}-${R}]${C} Installing Sudo..."${W}
+    apt update -y
+    apt install sudo -y
+    apt install wget apt-utils locales-all dialog -y
+    echo -e "\n${R} [${W}-${R}]${G} Sudo Successfully Installed !"${W}
+}
+
+login() {
+    banner
+    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Username [Lowercase] : \e[0m\e[1;96m\en' user
+    echo -e "${W}"
+    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Password : \e[0m\e[1;96m\en' pass
+    echo -e "${W}"
+    useradd -m -s $(which bash) ${user}
+    echo "${user}:${pass}" | chpasswd
+    echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/$user
+    echo "kali ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/kali
+    echo "proot-distro login --user $user nethunter --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp --fix-low-ports" > /data/data/com.termux/files/usr/bin/nethunter
+    chmod +x /data/data/com.termux/files/usr/bin/nethunter 
+    clear
+    echo
+    echo -e "\n${R} [${W}-${R}]${G} Restart your Termux & Type ${C}nethunter"${W}
+    echo
+
+}
+
+banner
+sudo
+login
