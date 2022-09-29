9ae159b3efe051a642d6604a1eb64316134af953
jdstryn <36963160+jdstryn@users.noreply.github.com>
2022-09-29 12:57:02 +0800

    Create nethunter.sh

---
+++ distro-plugins/nethunter.sh
@@ -0,0 +1,21 @@
+# This is a default distribution plug-in.
+
+# Do not modify this file as your changes will be overwritten on next update.
+
+# If you want customize installation, please make a copy.
+
+DISTRO_NAME="nethunter"
+
+DISTRO_COMMENT="Only for arm64 device."
+
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/kali-2022.3/rootfs/kalifs-arm64-minimal.tar.xz"
+
+TARBALL_SHA256['aarch64']="6e07c219fd3228d7c018450c50a26b1ab4af1c5e4c4df46e0ea86ecbefddb0a6"
+
+distro_setup() {
+
+# start ssh service automatically
+
+run_proot_cmd sudo service ssh start
+
+}
