c755946e5df04be1d6b59e5fae0838c7f20a4460
jdstryn <36963160+jdstryn@users.noreply.github.com>
2022-10-04 00:36:49 +0800

    Add files via upload

--- distro-plugins/nethunter.sh
+++ distro-plugins/nethunter.sh
@@ -2,7 +2,7 @@
 # Do not modify this file as your changes will be overwritten on next update.
 # If you want customize installation, please make a copy.
 
-DISTRO_NAME="nethunter"
+DISTRO_NAME="Nethunter v2022.3"
 DISTRO_COMMENT="Only for arm64 device."
-TARBALL_URL['aarch64']="https://kali.download/nethunter-images/kali-2022.3/rootfs/kalifs-arm64-minimal.tar.xz"
+TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-minimal.tar.xz"
 TARBALL_SHA256['aarch64']="6e07c219fd3228d7c018450c50a26b1ab4af1c5e4c4df46e0ea86ecbefddb0a6"
